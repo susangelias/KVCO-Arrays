6b8465c7defe4da6de1a5392005615d8be1137bb
Susan Elias <susangelias@gmail.com>
2015-02-03 17:31:50 -0500

    Initial Commit

---
+++ KVCODemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KVCODemo
+//
+//  Created by Susan Elias on 2/3/15.
+//  Copyright (c) 2015 GriffTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KVCODemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KVCODemo
+//
+//  Created by Susan Elias on 2/3/15.
+//  Copyright (c) 2015 GriffTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
